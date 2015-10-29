cc9404b08bc267008a5249afbf2858b26ab45499
Gilmour <horriganb@gilmour.org>
2015-10-29 08:57:33 -0400

    Stop and Forward

--- Pigo.py
+++ Pigo.py
@@ -1,10 +1,29 @@
-#from gopigo import *
+from gopigo import *
+import time
 
 __author__ = 'holtzz'
 
 class Pigo:
 
+    isMoving = False
+    servoPos = 90
+
     def __init__(self):
         print "Hello!"
 
+    def stop(self):
+        self.isMoving = False
+        while stop() != 1
+            time.sleep(.1)
+            print "Oops! I'm sorry! I just can't stop!"
+
+    def fwd(self):
+        self.isMoving = True
+        while fwd() != 1
+            time.sleep(.1)
+            print "I tried to vroom...but I couldn't."
+
 piggy = Pigo()
+tina.fwd()
+time.sleep(2)
+tina.stop()
